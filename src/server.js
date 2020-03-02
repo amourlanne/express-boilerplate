ed3fb76c43466beb715a48d85d94492d3eaa1d83
Alexis Mourlanne <alexis.mourlanne@gmail.com>
2020-03-02 19:48:16 +0100

    add env file

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+if(process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
 const server = express();
 
 server.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-server.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port ' + process.env.PORT || 3000)
+server.listen(process.env.PORT, async function () {
+  console.log('Example app listening on port ' + process.env.PORT)
 });
