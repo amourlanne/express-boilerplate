3e9b4e1399401aa643f932a072894e1e876fd4ee
Alexis Mourlanne <alexis.mourlanne@gmail.com>
2020-03-02 19:28:30 +0100

    change port

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ server.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-server.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+server.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port ' + process.env.PORT || 3000)
 });
